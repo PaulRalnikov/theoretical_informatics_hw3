a8ea560c761d8db60c5da10e5f822164151e3bac
DungeonMaster <st106332@student.spbu.ru>
2023-10-29 20:20:50 +0300

    Fixed path bugs

--- AllTasks/src/test/kotlin/testTask2.kt
+++ AllTasks/src/test/kotlin/testTask2.kt
@@ -10,10 +10,10 @@ internal class TestTask2 {
     private val sample = Task2()
 
     private val firstTestPath = "src/test/data/task2_test1.txt"
-    private val secondTestPath = "src/test/data/task1_test2.txt"
+    private val secondTestPath = "src/test/data/task2_test2.txt"
 
-    private val firstTestExpectedPath = "src/test//data/task2_test1_expected.txt"
-    private val secondTestExpectedPath = "src/test//data/task2_test2_expected.txt"
+    private val firstTestExpectedPath = "src/test/data/task2_test1_expected.txt"
+    private val secondTestExpectedPath = "src/test/data/task2_test2_expected.txt"
 
 
     //NFA tests
@@ -27,7 +27,8 @@ internal class TestTask2 {
 
     @Test
     fun test_2() {
-        val tempPath = kotlin.io.path.createTempFile().toString()
+//        val tempPath = kotlin.io.path.createTempFile().toString()
+        val tempPath = "src/test/data/temp.txt"
         sample.make_dfa_from_nfa(secondTestPath, tempPath)
         val result = isEqual(File(secondTestExpectedPath), File(tempPath))
         assert (result)
